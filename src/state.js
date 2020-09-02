0f29e339db869bb6ea65771eb2bae5f06ed25af5
denisekgosnell <denisekgosnell@gmail.com>
2020-09-02 10:16:22 -0400

    dkg: state refactor

--- src/state.js
+++ src/state.js
@@ -1,41 +1,45 @@
 import constants from "./constants";
+import _ from "lodash";
 
 const initialState = {
   initialized: false,
   gameId: 0,
-  currentGame: {
-    currentState: "",
-    currentRound: 0,
-    currentQuestion: 0,
-    audienceSize: 0,
-    players: [],
-    rounds: [
-      {
-        id: 1,
-        type: "",
-        title: "Round One",
-        scoreMultiplier: 1,
-        questions: [
-          {
-            type: "",
-            content: "",
-            answers: [
-              {
-                name: "",
-                answer: "",
-                votes: [
-                  {
-                    name: "",
-                  },
-                ],
-                audienceVotes: 0,
-                score: 0,
-              },
-            ],
-          },
-        ],
-      },
-    ],
+  audienceSize: 0,
+  currentState: {
+    name: "", // loading, adding players, user voting, display votes...
+    roundId: 0,
+  },
+  players: {
+    1: {
+      name: "",
+    },
+  },
+  rounds: {
+    1: {
+      type: "",
+      title: "Round One",
+      roundState: "",
+      scoreMultiplier: 1,
+    },
+  },
+  questions: {
+    1: {
+      roundId: 0,
+      content: "",
+    },
+  },
+  answers: {
+    1: {
+      questionId: 0,
+      playerId: 0,
+      content: "",
+    },
+  },
+  votes: {
+    1: {
+      playerId: 0,
+      answerId: 0,
+    },
   },
 };
 
@@ -47,7 +51,7 @@ export default function reducer(state = initialState, action) {
         ...state,
         initialized: !state.initialized,
       };
-      
+
     case constants.INIT_GAME:
       // start a new game
       // game's current state should be "ADDING PLAYERS"
@@ -70,7 +74,7 @@ export default function reducer(state = initialState, action) {
           players: [
             ...state.currentGame.players,
             {
-              name: action.name,
+              name: action.payload.name,
               vip: !state.currentGame.players.length,
               totalScore: 0,
             },
@@ -103,7 +107,47 @@ export default function reducer(state = initialState, action) {
       // incriments the audience state value
       return {
         ...state,
-        audienceSize: state.audienceSize++
+        audienceSize: state.audienceSize++,
+      };
+
+    case constants.QUESTION_ROUND:
+      // add in a QUESTION round
+      return {
+        ...state,
+        currentGame: {
+          ...state.currentGame,
+          rounds: [
+            ...state.currentGame.rounds,
+            // TODO: we are adding a new round of type "QUESTION"
+            // do I have to populate the full JSON body?
+          ],
+        },
+      };
+    case constants.ADDING_ANSWERS:
+      // Flip the bit of the current round to "ADDING ANSWERS"
+      return {
+        ...state,
+        currentGame: {
+          ...state.currentGame,
+          rounds: [
+            ...state.currentGame.rounds,
+            // TODO: we are changing the state of the current round to "ADDING ANSWERS"
+            // how do we flip this for the current round only?
+          ],
+        },
+      };
+
+    case constants.USER_VOTING:
+      // insert a player's vote into the state
+      return {
+        ...state,
+        votes: {
+          ...state.votes,
+          [action.payload.voteId]: {
+            questionId: action.payload.questionId,
+            playerId: action.payload.playerId,
+          },
+        },
       };
     default:
       return state;
