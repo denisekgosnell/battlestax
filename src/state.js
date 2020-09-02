52436715d54e912c6396f89a05b4eb732a2aca9e
denisekgosnell <denisekgosnell@gmail.com>
2020-09-02 12:37:57 -0400

    DKG: updating reducers

--- src/state.js
+++ src/state.js
@@ -3,44 +3,14 @@ import _ from "lodash";
 
 const initialState = {
   initialized: false,
-  gameId: 0,
+  gameId: "",
+  currentState: {}, // name, roundId
+  players: {}, // plauyerId, name, score; number of players == number of questions
   audienceSize: 0,
-  currentState: {
-    name: "", // loading, adding players, user voting, display votes...
-    roundId: 0,
-  },
-  players: {
-    1: {
-      name: "",
-    },
-  },
-  rounds: {
-    1: {
-      type: "",
-      title: "Round One",
-      roundState: "",
-      scoreMultiplier: 1,
-    },
-  },
-  questions: {
-    1: {
-      roundId: 0,
-      content: "",
-    },
-  },
-  answers: {
-    1: {
-      questionId: 0,
-      playerId: 0,
-      content: "",
-    },
-  },
-  votes: {
-    1: {
-      playerId: 0,
-      answerId: 0,
-    },
-  },
+  rounds: {}, // roundId, type, title, score multiplier
+  questions: {}, // questionId, roundId, content
+  answers: {}, // answerId, questionId, playerId, content
+  votes: {}, // voteId, answerId, playerId
 };
 
 export default function reducer(state = initialState, action) {
@@ -53,56 +23,126 @@ export default function reducer(state = initialState, action) {
       };
 
     case constants.INIT_GAME:
-      // start a new game
-      // game's current state should be "ADDING PLAYERS"
+      // start a new game;
+      // set the gameID from the action
+      // game's current state is "ADDING PLAYERS"
       return {
         ...state,
-        currentGame: {
-          currentState: constants.ADDING_PLAYERS,
+        gameId: action.payload.gameId,
+        currentState: {
+          ...state.currentState,
+          name: constants.ADDING_PLAYERS,
         },
       };
 
-    case constants.ADD_PLAYER:
+    case constants.ADDING_PLAYERS:
       // adds a new player to the players list
-      if (state.currentGame.players.length === constants.MAXIMUM_PLAYERS)
-        // TODO: ADDING THE PLAYER WHO TRIGGERS THIS LOGIC TO THE AUDIENCE
+      if (_.size(state.players.Data) === constants.MAXIMUM_PLAYERS)
         return { ...state, audienceSize: state.audienceSize++ };
       return {
         ...state,
-        currentGame: {
-          ...state.currentGame,
-          players: [
-            ...state.currentGame.players,
-            {
-              name: action.payload.name,
-              vip: !state.currentGame.players.length,
-              totalScore: 0,
-            },
-          ],
+        players: {
+          ...state.players,
+          [action.payload.playerId] : {
+            name: action.payload.name,
+            vip: !_.size(state.players.Data),
+            score: 0,
+          },
         },
       };
+
     case constants.START_TUTORIAL:
-      // flips the initialized state for the game payload
-      // game's current state should be "TUTORIAL ROUND"
+      // game's current round is "TUTORIAL ROUND"
+      // questions payload is initialized from the action
       return {
         ...state,
-        currentGame: {
-          ...state.currentGame,
-          currentRound: constants.TUTORIAL_ROUND,
+        currentState: {
+          ...state.currentState,
+          name: constants.TUTORIAL_ROUND,
         },
+        questions: action.payload.questions
       };
 
     case constants.ROUND_ONE:
-      // flips the initialized state for the game payload
-      // game's current state should be "TUTORIAL ROUND"
+      // game's current round should be "ROUND ONE"
       return {
         ...state,
-        currentGame: {
-          ...state.currentGame,
-          currentRound: constants.ROUND_ONE,
+        currentState: {
+          ...state.currentState,
+          roundId: constants.ROUND_ONE
         },
       };
 
+    case constants.ROUND_TWO:
+      // game's current round should be "ROUND TWO"
+      return {
+        ...state,
+        currentState: {
+          ...state.currentState,
+          roundId: constants.ROUND_TWO
+        },
+      };
+
+    case constants.FINAL_ROUND:
+      // game's current round should be "FINAL_ROUND"
+      return {
+        ...state,
+        currentState: {
+          ...state.currentState,
+          roundId: constants.FINAL_ROUND
+        },
+      };
+      
+    case constants.ANSWER_QUESTIONS:
+        // game's current state should be "ANSWER_QUESTIONS"
+        return {
+          ...state,
+          currentState: {
+            ...state.currentState,
+            name: constants.ANSWER_QUESTIONS
+          },
+        };
+  
+    case constants.PLAYER_VOTING:
+      // game's current state should be "PLAYER_VOTING"
+      return {
+        ...state,
+        currentState: {
+          ...state.currentState,
+          name: constants.PLAYER_VOTING
+        },
+      };
+    
+    case constants.AUDIENCE_VOTING:
+      // game's current state should be "AUDIENCE_VOTING"
+      return {
+        ...state,
+        currentState: {
+          ...state.currentState,
+          name: constants.AUDIENCE_VOTING
+        },
+      };
+      
+    case constants.UPDATE_SCORES:
+      // game's current state should be "UPDATE_SCORES"
+      return {
+        ...state,
+        currentState: {
+          ...state.currentState,
+          name: constants.UPDATE_SCORES
+        },
+      };
+        
+    case constants.UPDATE_LEADERBOARD:
+      // game's current state should be "UPDATE_LEADERBOARD"
+      return {
+        ...state,
+        currentState: {
+          ...state.currentState,
+          name: constants.UPDATE_LEADERBOARD
+        },
+      };              
+    
     case constants.ADD_AUDIENCE:
       // incriments the audience state value
       return {
@@ -110,45 +150,32 @@ export default function reducer(state = initialState, action) {
         audienceSize: state.audienceSize++,
       };
 
-    case constants.QUESTION_ROUND:
-      // add in a QUESTION round
-      return {
-        ...state,
-        currentGame: {
-          ...state.currentGame,
-          rounds: [
-            ...state.currentGame.rounds,
-            // TODO: we are adding a new round of type "QUESTION"
-            // do I have to populate the full JSON body?
-          ],
-        },
-      };
-    case constants.ADDING_ANSWERS:
-      // Flip the bit of the current round to "ADDING ANSWERS"
-      return {
-        ...state,
-        currentGame: {
-          ...state.currentGame,
-          rounds: [
-            ...state.currentGame.rounds,
-            // TODO: we are changing the state of the current round to "ADDING ANSWERS"
-            // how do we flip this for the current round only?
-          ],
-        },
-      };
+    // case constants.ADDING_ANSWERS:
+    //   // Flip the bit of the current round to "ADDING ANSWERS"
+    //   return {
+    //     ...state,
+    //     currentGame: {
+    //       ...state.currentGame,
+    //       rounds: [
+    //         ...state.currentGame.rounds,
+    //         // TODO: we are changing the state of the current round to "ADDING ANSWERS"
+    //         // how do we flip this for the current round only?
+    //       ],
+    //     },
+    //   };
 
-    case constants.USER_VOTING:
-      // insert a player's vote into the state
-      return {
-        ...state,
-        votes: {
-          ...state.votes,
-          [action.payload.voteId]: {
-            questionId: action.payload.questionId,
-            playerId: action.payload.playerId,
-          },
-        },
-      };
+    // case constants.USER_VOTING:
+    //   // insert a player's vote into the state
+    //   return {
+    //     ...state,
+    //     votes: {
+    //       ...state.votes,
+    //       [action.payload.voteId]: {
+    //         questionId: action.payload.questionId,
+    //         playerId: action.payload.playerId,
+    //       },
+    //     },
+    //   };
     default:
       return state;
   }
--- src/state.test.js
+++ src/state.test.js
@@ -1,12 +1,10 @@
 import reducer, { initialState } from "./state";
-import constants from "./constants"
+import constants from "./constants";
 
 describe("start a new game reducer", () => {
   // TEST: testing the default returns the initialized state
   it("should return the initialized state; testing the default", () => {
-    expect(
-      reducer(initialState, {})
-    ).toMatchSnapshot();
+    expect(reducer(initialState, {})).toMatchSnapshot();
   });
 
   // TEST: initialize the app
@@ -26,12 +24,12 @@ describe("start a new game reducer", () => {
       })
     ).toMatchSnapshot();
   });
-  
+
   // TEST: should add 1 player, and that player should be the vip
-  it("should handle ADD_PLAYER request", () => {
+  it("should handle ADDING_PLAYERS request", () => {
     expect(
       reducer(initialState, {
-        type: constants.ADD_PLAYER,
+        type: constants.ADDING_PLAYERS,
       })
     ).toMatchSnapshot();
   });
@@ -54,6 +52,69 @@ describe("start a new game reducer", () => {
     ).toMatchSnapshot();
   });
 
+  // TEST: should start round two
+  it("should handle ROUND_TWO request", () => {
+    expect(
+      reducer(initialState, {
+        type: constants.ROUND_TWO,
+      })
+    ).toMatchSnapshot();
+  });
+
+  // TEST: should start final round
+  it("should handle FINAL_ROUND request", () => {
+    expect(
+      reducer(initialState, {
+        type: constants.FINAL_ROUND,
+      })
+    ).toMatchSnapshot();
+  });
+
+  // TEST: should change current state to ANSWER_QUESTIONS
+  it("should handle ANSWER_QUESTIONS request", () => {
+    expect(
+      reducer(initialState, {
+        type: constants.ANSWER_QUESTIONS,
+      })
+    ).toMatchSnapshot();
+  });
+
+  // TEST: should change current state to PLAYER_VOTING
+  it("should handle PLAYER_VOTING request", () => {
+    expect(
+      reducer(initialState, {
+        type: constants.PLAYER_VOTING,
+      })
+    ).toMatchSnapshot();
+  });
+
+  // TEST: should change current state to AUDIENCE_VOTING
+  it("should handle AUDIENCE_VOTING request", () => {
+    expect(
+      reducer(initialState, {
+        type: constants.AUDIENCE_VOTING,
+      })
+    ).toMatchSnapshot();
+  });
+
+  // TEST: should change current state to UPDATE_SCORES
+  it("should handle UPDATE_SCORES request", () => {
+    expect(
+      reducer(initialState, {
+        type: constants.UPDATE_SCORES,
+      })
+    ).toMatchSnapshot();
+  });
+
+  // TEST: should change current state to UPDATE_LEADERBOARD
+  it("should handle UPDATE_LEADERBOARD request", () => {
+    expect(
+      reducer(initialState, {
+        type: constants.UPDATE_LEADERBOARD,
+      })
+    ).toMatchSnapshot();
+  });
+
   // TEST: add to the audience
   it("should handle ADD_AUDIENCE request", () => {
     expect(
@@ -63,13 +124,16 @@ describe("start a new game reducer", () => {
     ).toMatchSnapshot();
   });
 
+  // TODO HERE
+  // TEST: adding a question round
+
+  // TEST: adding a final round
+
+  // TEST: changing the current round's state to "ADDING ANSWERS"
+
   // **Integration tests**
   // TEST: Should not start a game with 1 or 2 players
   // TEST 6: should start a game with no more than 8 players
-  // TEST 7: should allow audience members
-  // TEST 8: should allow the game to start
-  // TEST 9: should move to the game tutorial
-  // TEST 10: should move to a question round
   // TEST 11: should receive question responses from players
   // TEST 12: should move to a voting round
   // TEST 13: should receive votes from players
