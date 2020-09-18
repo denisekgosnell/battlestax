1d58de803f4727927363321a036b3cd1db088a57
Chris Wilhite <chris.wilhite@datastax.com>
2020-09-18 09:54:53 -0400

    make paths relative

--- src/api.js
+++ src/api.js
@@ -6,37 +6,29 @@ export const createGame = async (gameId) => {
     "currentPlayerId",
     "initialized",
   ]);
-  const res = await fetch(
-    `http://localhost:8888/.netlify/functions/insertGame/${gameId}`,
-    {
-      method: "POST",
-      body: JSON.stringify({ ...filteredGame, gameId }),
-    }
-  );
+  const res = await fetch(`/.netlify/functions/insertGame/${gameId}`, {
+    method: "POST",
+    body: JSON.stringify({ ...filteredGame, gameId }),
+  });
   return await res.json();
 };
 
 export const getGame = async (path) => {
-  const res = await fetch(
-    `http://localhost:8888/.netlify/functions/getGame/${path}`
-  );
+  const res = await fetch(`/.netlify/functions/getGame/${path}`);
   return await res.json();
 };
 
 export const updateGame = async (path, data) => {
-  const res = await fetch(
-    `http://localhost:8888/.netlify/functions/updateGame/${path}`,
-    {
-      method: "PATCH",
-      body: JSON.stringify(data),
-    }
-  );
+  const res = await fetch(`/.netlify/functions/updateGame/${path}`, {
+    method: "PATCH",
+    body: JSON.stringify(data),
+  });
   return await res.json();
 };
 
 export const addPlayer = async (gameId, playerId) => {
   const res = await fetch(
-    `http://localhost:8888/.netlify/functions/updateGame/${gameId}/players/${playerId}`,
+    `/.netlify/functions/updateGame/${gameId}/players/${playerId}`,
     {
       method: "PUT",
       body: JSON.stringify({ name: playerId }),
